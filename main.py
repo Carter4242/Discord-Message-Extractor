24866b0efb83666562fe118cf983d838fe5e2eff
Carter4242 <92061520+Carter4242@users.noreply.github.com>
2023-08-31 14:29:59 -0400

    Basic hardcoded compiling of all messages

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import csv
+        
+input = r"C:\Users\Carter\Desktop\!Downloads\Data - Google+Discord 8-2022\package"
+
+folder = os.path.join(input, "messages")
+
+print(folder)
+
+messages = 0
+empty = 0
+runs = 0
+with open("demofile2.txt", "w", encoding="utf8") as demo:
+    for root, dirs, files in os.walk(folder):
+        if ("messages.csv" in files):
+            with open(os.path.join(root, "messages.csv"), 'r', encoding="utf8") as f:
+                reader = csv.reader(f)
+                next(reader) # Ignore first row
+                for row in reader:
+                    messages += 1
+                    if (row[2] != ""):
+                        demo.write(row[2] + "\n")
+                    else:
+                        empty += 1
+
+            print (runs, messages)
+            runs += 1
+print(messages, empty)
